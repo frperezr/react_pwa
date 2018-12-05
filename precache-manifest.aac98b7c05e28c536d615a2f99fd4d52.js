271e3695fb51b7b8dc7fad07b72657adb819b0ee
Francisco Pérez <frperezr93@gmail.com>
2018-12-05 15:38:44 -0300

    Updates

---
+++ precache-manifest.aac98b7c05e28c536d615a2f99fd4d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f642e8a84b9c345215c6",
+    "url": "/react_pwa/static/js/runtime~main.f642e8a8.js"
+  },
+  {
+    "revision": "c7de42211f4a5741125a",
+    "url": "/react_pwa/static/js/main.c7de4221.chunk.js"
+  },
+  {
+    "revision": "2f9fbbe7a99ae29dfa8a",
+    "url": "/react_pwa/static/js/1.2f9fbbe7.chunk.js"
+  },
+  {
+    "revision": "c7de42211f4a5741125a",
+    "url": "/react_pwa/static/css/main.f0586804.chunk.css"
+  },
+  {
+    "revision": "2f9fbbe7a99ae29dfa8a",
+    "url": "/react_pwa/static/css/1.e1e938e3.chunk.css"
+  },
+  {
+    "revision": "2fa900c97773df61e73c8b0fe466f9a2",
+    "url": "/react_pwa/index.html"
+  }
+];
